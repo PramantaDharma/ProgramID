af9df2c26a4081030a9a4a2ea85149be041936c0
sandiartakadek <sandiarta435@gmail.com>
2022-10-27 07:46:00 +0800

    edit view kategory + nambah gambar

--- view/home/category.php
+++ view/home/category.php
@@ -50,7 +50,8 @@
                     <div class="dropdown-menu">
                     <a class="dropdown-item" href="#">Action</a>
                     <a class="dropdown-item" href="#">Another action</a>
-                    <div class="dropdown-divider"></div>
+                    <div class="dropdown-divider"></di
+                    v>
                     <a class="dropdown-item" href="#">Something else here</a>
                     </div>
                 </li> -->
@@ -268,29 +269,29 @@
                 </nav>
                 <!-- End of Topbar -->
                 <!-- Begin Page Content -->
-                <div class="container-fluid">
+                <div class="container-fluid ">
                     <!-- Page Heading -->
-                    <div class="d-flex-column mt-2 mb-4 m-4">
-                        <h1 class="text-primary font-weight-bold">Program ID</h1>
+                    <div class="d-flex-column mt-2 mb-4 m-5">
+                        <h1 class="text-success font-weight-bold">Program ID</h1>
                         <h4 class="text-dark mt-2">Bantu pemula belajar koding (coding) dengan tutorial
                             <br> yang mudah dipahami.</h4>
                     </div>
-                    <div class="jn d-flex-column mt-2 m-4">
+                    <div class="jn d-flex-column mt-2 m-5">
                         <a href="#" data-turbo="false" class="px-3 py-2 border rounded-lg" style="background-color: #f5f5f5;">
                             Join Newsletter</a></div>
                     </div>
                     <!-- Content Row -->
-                    <div class="row justify-content-between m-3"> 
+                    <div class="row justify-content-between m-4"> 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-6 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card border-left-success shadow h-100 py-2">
                                 <div class="card-body">
                                 <div class="col-auto">
                                             <img src="../../public/asset/img/programming-language 1.png" alt="" width="80">
                                         </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="font-weight-bold text-primary mb-1">
+                                            <div class="font-weight-bold text-success mb-1">
                                                 Web Programming</div>
                                             <p class="mb-0 font-weight-bold">Baca tutorial dasar web programming, mulai dari HTML, CSS, JS, PHP, MySQL, Codeigniter, React, dan masih banyak lagi</p>
                                         </div>
@@ -300,14 +301,14 @@
                         </div>
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-6 col-md-6 mb-4">
-                            <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card border-left-success shadow h-100 py-2">
                                 <div class="card-body">
                                 <div class="col-auto">
                                             <img src="../../public/asset/img/android.png" alt="" width="80">
                                         </div>
                                     <div class="row no-gutters align-items-center">
                                         <div class="col mr-2">
-                                            <div class="font-weight-bold text-primary mb-1">
+                                            <div class="font-weight-bold text-success mb-1">
                                                 Android Programming</div>
                                             <p class="mb-0 font-weight-bold">Baca tutorial android programming mulai dari JAVA, XML, Android SDK, Android Studio, API, SQLite</p>
                                         </div>
@@ -321,15 +322,15 @@
 
                 </div>
 
-                <div class="up d-flex-column mt-2 mb-4 m-4">
-                    <p class="text-primary text-uppercase">Update</p>
+                <div class="up d-flex-column mt-2 mb-4 m-5">
+                    <p class="text-success text-uppercase">Update</p>
                     <h4 class="text-dark mt-2"><strong>Ingin belajar apa hari ini?</strong></h4>
 
                  </div>
-                    <div class="row justify-content-between m-3">    
-                        <div class="d-flex mb-4">
+                    <div class="row justify-content-between m-4">    
+                        <div class="d-flex mb-1">
                             <!-- Illustrations -->
-                            <div class="card shadow mb-4 mx-3">
+                            <div class="card shadow mb-2 mx-3">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Tutorial HTML dasar</h6>
                                 </div>
@@ -344,7 +345,7 @@
                                 </div>
                             </div>
                             <!-- Illustrations -->
-                            <div class="card shadow mb-4 mx-3">
+                            <div class="card shadow mb-2 mx-3">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Tutorial PHP dasar</h6>
                                 </div>
@@ -359,7 +360,7 @@
                                 </div>
                             </div>
                             <!-- Illustrations -->
-                            <div class=" card shadow mb-4 mx-3">
+                            <div class=" card shadow mb-2 mx-3">
                                 <div class="card-header py-3">
                                     <h6 class="m-0 font-weight-bold text-primary">Tutorial JS dasar</h6>
                                 </div>
@@ -373,14 +374,70 @@
                                     <a target="_blank" rel="nofollow" href="https://undraw.co/">Baca selengkapnya! &rarr;</a>
                                 </div>
                             </div>
-   
+        
                         </div>
-                    </div>     
+                    </div> 
+                    <div class="row justify-content-between m-3">    
+                        <div class="d-flex mb-2">
+                            <!-- Illustrations -->
+                            <div class="card shadow mb-2 mx-3">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Tutorial Python dasar</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid mt-3 mb-4" style="width: 25rem;"
+                                            src="../../public/asset/img/python-sm.png" alt="...">
+                                    </div>
+                                    <p>Add some quality, svg illustrations to your project courtesy of unDraw, a
+                                        constantly updated collection of beautiful!</p>
+                                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Baca selengkapnya! &rarr;</a>
+                                </div>
+                            </div>
+                            <!-- Illustrations -->
+                            <div class=" card shadow mb-2 mx-3">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Tutorial Reactjs dasar</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid mt-3 mb-4" style="width: 25rem;"
+                                            src="../../public/asset/img/reactjs-sm.png" alt="...">
+                                    </div>
+                                    <p>Add some quality, svg illustrations to your project courtesy of unDraw, a
+                                        constantly updated collection of beautiful!</p>
+                                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Baca selengkapnya! &rarr;</a>
+                                </div>
+                            </div>
+                            <!-- Illustrations -->
+                            <div class=" card shadow mb-2 mx-3">
+                                <div class="card-header py-3">
+                                    <h6 class="m-0 font-weight-bold text-primary">Tutorial Kotlin dengan Android Studio</h6>
+                                </div>
+                                <div class="card-body">
+                                    <div class="text-center">
+                                        <img class="img-fluid mt-3 mb-4" style="width: 25rem;"
+                                            src="../../public/asset/img/kotlin-sm.png" alt="...">
+                                    </div>
+                                    <p>Add some quality, svg illustrations to your project courtesy of unDraw, a
+                                        constantly updated collection of beautiful!</p>
+                                    <a target="_blank" rel="nofollow" href="https://undraw.co/">Baca selengkapnya! &rarr;</a>
+                                </div>
+                            </div>    
                 </div>
                 <!-- /.container-fluid -->
             </div>
             <!-- End of Main Content -->
             <!-- Footer -->
+            <footer class="footer-materi sticky-footer">
+                <div class="container my-auto">
+                    <div class="copyright text-center my-auto">
+                        <span>Copyright &copy; Your Website 2021</span>
+                    </div>
+                </div>
+            </footer>
+            <!-- End of Footer -->
+
             
         </div>
         <!-- End of Content Wrapper -->
