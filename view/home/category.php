25d7802f7a91d412ade93957ac68fb9f7acdbf2b
sandiartakadek <sandiarta435@gmail.com>
2022-10-31 12:10:07 +0800

    menambah view css kategori

--- view/home/category.php
+++ view/home/category.php
@@ -66,57 +66,7 @@
             </div>
         </div>
     </nav>
-    <nav class="navbar-category navbar navbar-expand navbar-light topbar static-top shadow">
 
-        <!-- Sidebar Toggle (Topbar) -->
-        <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-            <i class="fa fa-bars"></i>
-        </button>
-        
-        <div class="navbar-brand nav-cat-mat">
-            <a href="" class="mr-4"><i class="bi bi-house-door-fill"></i></a>
-            <a class="" href="#"></a>
-            <a href=""></a>
-            <a href=""></a>
-        </div>
-
-        
-        <!-- Topbar Navbar -->
-        <ul class="navbar-nav ml-auto">
-
-            <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-            <li class="nav-item dropdown no-arrow d-sm-none">
-                <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fas fa-search fa-fw"></i>
-                </a>
-                <!-- Dropdown - Messages -->
-                <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
-                    <form class="form-inline mr-auto w-100 navbar-search">
-                        <div class="input-group">
-                            <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
-                            <div class="input-group-append">
-                                <button class="btn btn-primary" type="button">
-                                    <i class="fas fa-search fa-sm"></i>
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </li>
-
-            <div class="topbar-divider d-none d-sm-block"></div>
-
-            <!-- Nav Item - User Information -->
-            <li class="nav-item dropdown no-arrow">
-                <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    <i class="fa fa-moon"></i>
-                    <!-- <img class="img-profile rounded-circle" src="../../public/asset/img/undraw_profile.svg"> -->
-                </a>
-            </li>
-
-        </ul>
-
-    </nav>
     
     <!-- Page Wrapper -->
     <div id="wrapper">
@@ -128,186 +78,28 @@
             <!-- Main Content -->
             <div id="content">
                 <!-- Topbar -->
-                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
-                    <!-- Sidebar Toggle (Topbar) -->
-                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
-                        <i class="fa fa-bars"></i>
-                    </button>
-                    <!-- Topbar Search -->
-                    
-                    <!-- Topbar Navbar -->
-                    <ul class="navbar-nav ml-auto">
-                        <!-- Nav Item - Search Dropdown (Visible Only XS) -->
-                        <li class="nav-item dropdown no-arrow d-sm-none">
-                            <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"
-                                data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <i class="fas fa-search fa-fw"></i>
-                            </a>
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in"
-                                aria-labelledby="searchDropdown">
-                                <form class="form-inline mr-auto w-100 navbar-search">
-                                    <div class="input-group">
-                                        <input type="text" class="form-control bg-light border-0 small"
-                                            placeholder="Search for..." aria-label="Search"
-                                            aria-describedby="basic-addon2">
-                                        <div class="input-group-append">
-                                            <button class="btn btn-primary" type="button">
-                                                <i class="fas fa-search fa-sm"></i>
-                                            </button>
-                                        </div>
-                                    </div>
-                                </form>
-                            </div>
-                        </li>
-                        <!-- Nav Item - Alerts -->
-            
-                            <!-- Dropdown - Alerts -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="alertsDropdown">
-                                <h6 class="dropdown-header">
-                                    Alerts Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-primary">
-                                            <i class="fas fa-file-alt text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 12, 2019</div>
-                                        <span class="font-weight-bold">A new monthly report is ready to download!</span>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-success">
-                                            <i class="fas fa-donate text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 7, 2019</div>
-                                        $290.29 has been deposited into your account!
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="mr-3">
-                                        <div class="icon-circle bg-warning">
-                                            <i class="fas fa-exclamation-triangle text-white"></i>
-                                        </div>
-                                    </div>
-                                    <div>
-                                        <div class="small text-gray-500">December 2, 2019</div>
-                                        Spending Alert: We've noticed unusually high spending for your account.
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Show All Alerts</a>
-                            </div>
-                        </li>
-                        <!-- Nav Item - Messages -->
                 
-                            <!-- Dropdown - Messages -->
-                            <div class="dropdown-list dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="messagesDropdown">
-                                <h6 class="dropdown-header">
-                                    Message Center
-                                </h6>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_1.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div class="font-weight-bold">
-                                        <div class="text-truncate">Hi there! I am wondering if you can help me with a
-                                            problem I've been having.</div>
-                                        <div class="small text-gray-500">Emily Fowler · 58m</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_2.svg"
-                                            alt="...">
-                                        <div class="status-indicator"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">I have the photos that you ordered last month, how
-                                            would you like them sent to you?</div>
-                                        <div class="small text-gray-500">Jae Chun · 1d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="img/undraw_profile_3.svg"
-                                            alt="...">
-                                        <div class="status-indicator bg-warning"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Last month's report looks great, I am very happy with
-                                            the progress so far, keep up the good work!</div>
-                                        <div class="small text-gray-500">Morgan Alvarez · 2d</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item d-flex align-items-center" href="#">
-                                    <div class="dropdown-list-image mr-3">
-                                        <img class="rounded-circle" src="https://source.unsplash.com/Mv9hjnEUHR4/60x60"
-                                            alt="...">
-                                        <div class="status-indicator bg-success"></div>
-                                    </div>
-                                    <div>
-                                        <div class="text-truncate">Am I a good boy? The reason I ask is because someone
-                                            told me that people say this to all dogs, even if they aren't good...</div>
-                                        <div class="small text-gray-500">Chicken the Dog · 2w</div>
-                                    </div>
-                                </a>
-                                <a class="dropdown-item text-center small text-gray-500" href="#">Read More Messages</a>
-                            </div>
-                        </li>
-                        <div class="topbar-divider d-none d-sm-block"></div>
-                        <!-- Nav Item - User Information -->
-        
-                            <!-- Dropdown - User Information -->
-                            <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
-                                aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Settings
-                                </a>
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Activity Log
-                                </a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
-                                    <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Logout
-                                </a>
-                            </div>
-                        </li>
-                    </ul>
-                </nav>
                 <!-- End of Topbar -->
                 <!-- Begin Page Content -->
-                <div class="container-fluid ">
+        
+                <div class="container-fluid d-flex flex-column" style="background-color: #0D9488;">
                     <!-- Page Heading -->
-                    <div class="d-flex-column mt-2 mb-4 m-5">
-                        <h1 class="text-success font-weight-bold">Program ID</h1>
-                        <h4 class="text-dark mt-2">Bantu pemula belajar koding (coding) dengan tutorial
-                            <br> yang mudah dipahami.</h4>
+                    <div class="d-flex-column mt-2 mb-1 m-5">
+                        <h1 class="text-white font-weight-bold">Program ID</h1>
+                        <h4 class="text-white mt-2">Bantu pemula belajar koding (coding) dengan tutorial
+                        <br> yang mudah dipahami.</h4> 
+                        <div class="d-flex">
+                        <a href="#updatenewsletter" data-turbo="false" class="px-3 py-2 border rounded-lg text-white">Join
+                        Newsletter</a></div>
+                    </div>      
                     </div>
-                    <div class="jn d-flex-column mt-2 m-5">
-                        <a href="#" data-turbo="false" class="px-3 py-2 border rounded-lg" style="background-color: #f5f5f5;">
-                            Join Newsletter</a></div>
-                    </div>
-                    <!-- Content Row -->
-                    <div class="row justify-content-between m-4"> 
+                </div>
+                <div class="container-fluid d-flex flex-column" style="background-color: #0D9488;">
+                 <!-- Content Row -->
+                 <div class="row justify-content-between m-4"> 
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-6 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card shadow h-100 py-2">
                                 <div class="card-body">
                                 <div class="col-auto">
                                             <img src="../../public/asset/img/programming-language 1.png" alt="" width="80">
@@ -316,7 +108,7 @@
                                         <div class="col mr-2">
                                             <div class="font-weight-bold text-primary mb-1">
                                                 Web Programming</div>
-                                            <p class="mb-0 font-weight-bold">Baca tutorial dasar web programming, mulai dari HTML, CSS, JS, PHP, MySQL, Codeigniter, React, dan masih banyak lagi</p>
+                                            <p class="mb-0 font-weight-bold text-dark">Baca tutorial dasar web programming, mulai dari HTML, CSS, JS, PHP, MySQL, Codeigniter, React, dan masih banyak lagi</p>
                                         </div>
                                     </div>
                                 </div>
@@ -324,7 +116,7 @@
                         </div>
                         <!-- Earnings (Monthly) Card Example -->
                         <div class="col-xl-6 col-md-6 mb-4">
-                            <div class="card border-left-success shadow h-100 py-2">
+                            <div class="card shadow h-100 py-2">
                                 <div class="card-body">
                                 <div class="col-auto">
                                             <img src="../../public/asset/img/android.png" alt="" width="80">
@@ -333,24 +125,22 @@
                                         <div class="col mr-2">
                                             <div class="font-weight-bold text-primary mb-1">
                                                 Android Programming</div>
-                                            <p class="mb-0 font-weight-bold">Baca tutorial android programming mulai dari JAVA, XML, Android SDK, Android Studio, API, SQLite</p>
+                                            <p class="mb-0 font-weight-bold text-dark">Baca tutorial android programming mulai dari JAVA, XML, Android SDK, Android Studio, API, SQLite</p>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>   
                     </div>
-
-                <div>
-
                 </div>
-
-                <div class="up d-flex-column mt-2 mb-4 m-5">
+                   
+            
+                <div class="up d-flex-column mt-2 m-5" id="updatenewsletter">
                     <p class="text-success text-uppercase">Update</p>
                     <h4 class="text-dark mt-2"><strong>Ingin belajar apa hari ini?</strong></h4>
 
                  </div>
-                    <div class="row justify-content-between m-4">    
+                    <div class="row justify-content-between mt-0 m-4">    
                         <div class="d-flex mb-1">
                             <!-- Illustrations -->
                             <div class="card shadow mb-2 mx-3">
